db77335c56a34a447d0c5d6ca291768d712b706c
data-pup <kevinmartinucr@gmail.com>
2018-03-13 14:48:23 -0700

    Added an initial implementation of the ChessBoard type.

---
+++ src/ChessBoard.ts
@@ -0,0 +1,37 @@
+
+export type Position = [number, number];
+
+export type Queen = {
+    kind:'queen';
+    position:Position;
+}
+
+export class ChessBoard {
+    private static readonly invalidParamsError = 'Invalid constructor parameters!';
+    private static readonly invalidPositionError = 'Invalid position exists!';
+
+    private static validateDimensions(height:number, width:number) : void {
+        if (height <= 0 || width <= 0) { throw new Error(this.invalidParamsError); }
+    }
+
+    private static validatePositions(pieces:Queen[], height:number, width:number) : void {
+        pieces.forEach((p) => {
+            const [x, y]:Position = p.position;
+            if (x < 0 || y < 0 || x >= height || y >= height) {
+                throw new Error(this.invalidPositionError);
+            }
+        });
+    }
+
+    public readonly pieces:Queen[];
+    public readonly height:number;
+    public readonly width:number;
+
+    constructor(pieces:Queen[]=[], height:number=8, width:number=8) {
+        ChessBoard.validateDimensions(height, width);
+        ChessBoard.validatePositions(pieces, height, width);
+        this.pieces = new Array(...pieces);
+        this.height = height;
+        this.width = width;
+    }
+}
