58b9ff949a47f2d030a5efb884a7ee7bcf6492db
data-pup <kevinmartinucr@gmail.com>
2018-03-13 16:57:30 -0700

    Cleaned the position unique check.

--- src/ChessBoard.ts
+++ src/ChessBoard.ts
@@ -11,23 +11,18 @@ export class ChessBoard {
     }
 
     private static validatePositions(pieces:Queen[], height:number, width:number) : void {
-        pieces.forEach((p) => { // Check that each position is in bounds.
-            const [x, y]:Position = p.position;
-            if (x < 0 || y < 0 || x >= height || y >= height) {
-                throw new Error(this.invalidPositionError);
-            }
-        });
-
-        const positionsAreUnique = pieces // Check each position is unique.
-            .map((q:Queen) : Position => q.position)
-            .every((currPos:Position, i:number, arr:Position[]) : boolean =>
-                arr.filter( // Check that only one item in the position list matches.
-                    (evalPos:Position) : boolean =>
-                        currPos[0] === evalPos[0] && currPos[1] === evalPos[1],
-                ).length === 1, // If more than 1 position matched, there are duplicates.
-            );
-
-        if (!positionsAreUnique) { throw new Error(this.invalidPositionError); }
+        pieces.map((q:Queen) : Position => q.position) // Select each position.
+            .forEach(([x, y]:Position, i:number, arr:Position[]) => {
+                // const [x, y]:Position = currPos; // Check that the position is in bounds.
+                if (x < 0 || y < 0 || x >= height || y >= height) {
+                    throw new Error(this.invalidPositionError);
+                } // Check that only one piece is at the current position.
+                const onePieceAtCurrPos:boolean = arr
+                    .filter(([a, b]:Position) : boolean => x === a && y === b)
+                    .length === 1; // Throw an exception if two or more pieces share a position.
+                if (!onePieceAtCurrPos) { throw new Error(this.invalidPositionError); }
+            },
+        );
     }
 
     public static readonly invalidParamsError = 'Invalid constructor parameters!';
