d7f0bad54bb7457cbb6f76a35048341457e16123
data-pup <kevinmartinucr@gmail.com>
2018-03-14 17:49:50 -0700

    Removed previous implementation from the branch.

--- src/ChessBoard/lib/IChessBoard.ts
+++
@@ -1,9 +0,0 @@
-import { Queen } from './Queen';
-import { Position } from './Position';
-
-export interface IChessBoard {
-    readonly pieces:Queen[];
-    readonly height:number;
-    readonly width:number;
-    positionInBounds([x, y]:Position) : boolean;
-}
--- src/ChessBoard/lib/Queen.ts
+++
@@ -1,6 +0,0 @@
-import { Position } from './Position';
-
-export type Queen = {
-    name:'Q';
-    position:Position;
-};
